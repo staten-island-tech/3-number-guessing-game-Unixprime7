dda777c6a7cb377c5cbe4b9dad6a8e37fc811f09
Unixprime7 <michaeld801@nycstudents.net>
2026-02-10 21:14:28 -0500

    home extra

--- numberguesser400.py
+++ numberguesser400.py
@@ -4,3 +4,12 @@ randomnumber = random.randint(1,10)
 
 randomguess = input("Pick a number from 1-10 or else... ")
 guess_value = int(randomguess)
+
+while guess_value != randomnumber:
+    if guess_value > randomnumber:
+        closeness = "The number is a bit smaller, try again! "
+    else:
+        closeness = "The number is a bit bigger, try again! "
+    randomguess = input(closeness)
+    guess_value = int(randomguess)
+print("Nice you did it")
---
+++ whiletest.py
@@ -0,0 +1,6 @@
+order = 0
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!")
